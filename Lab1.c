4ce5f89ba375fcc0336a646b8781c028d0e7d139
Cristobal Abarca Olivares <cristoxea@localhost.localdomain>
2020-01-12 18:24:54 -0300

    Pequeños cambios que hacen el codigo funcional

--- Lab1.c
+++ Lab1.c
@@ -7,25 +7,35 @@ float* euler_method(int t0, int y0, double delta_t);
 
 
 int main(){
-  int t0 = 0, y0 = -1;
+  int t0;
+  int y0;
+  int i;
+  t0 = 0;
+  y0 = 4;
   double delta_t[6] = {pow(10,-1), pow(10,-2), pow(10,-3), pow(10,-4),
                     pow(10,-5), pow(10,-6)};
   float* y;
   y = euler_method(t0, y0, delta_t[0]);
-  printf("%f", y[0]);
+  for(i = 0 ; i < 10/delta_t[0]  ; i++)
+  {
+  	printf("y[%i]=%f\n", i, *(y+i));
+  }
+  printf("\n");
   free(y);
   return 0;
 }
 
+
 float* euler_method(int t0, int y0, double delta_t){
+  int i, j;
   int N = 10/delta_t;
-  double sum_exp = 1;
+  float sum_exp = 1;
   float* y = (float*) malloc(sizeof(float)*N); //Asignacion de memoria
-  for(int i = 1; i <= N; i++){
-    for(int j = 1; i<= i - 1; i++){
+  for(i = 0 ; i < N ; i++){
+    for(j = 0 ; j <= i - 1 ; j++){
       sum_exp += exp(-j*delta_t);
     }
-    y[i] = -1 + float(delta_t*sum_exp);
+    y[i] = -y0 + (delta_t*sum_exp);
   }
   return y;
 }
