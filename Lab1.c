62a1c46a9632cd411a9493408768c5189086f3fa
Cristobal Abarca Olivares <cristoxea@localhost.localdomain>
2020-01-12 19:18:21 -0300

    funciono

--- Lab1.c
+++ Lab1.c
@@ -3,39 +3,49 @@
 #include<ctype.h>
 #include<math.h>
 
-float* euler_method(int t0, int y0, double delta_t);
-
-
+float* euler_method(int t0, int y0, float delta_t);
+float edo_original(float t);
+float edo_resuelta(float t);
 int main(){
   int t0;
   int y0;
   int i;
   t0 = 0;
   y0 = 4;
-  double delta_t[6] = {pow(10,-1), pow(10,-2), pow(10,-3), pow(10,-4),
+  float delta_t[6] = {pow(10,-1), pow(10,-2), pow(10,-3), pow(10,-4),
                     pow(10,-5), pow(10,-6)};
   float* y;
   y = euler_method(t0, y0, delta_t[0]);
-  for(i = 0 ; i < 10/delta_t[0]  ; i++)
+  for(i = 0 ; i <= 10/delta_t[0] - 10  ; i++)
   {
-  	printf("y[%i]=%f\n", i, *(y+i));
+  	printf("%f\n", (i + 10)*delta_t[0]);
+  	printf("y[%i]=%f   ,   %f\n", i, *(y+i), edo_resuelta((i + 10)*delta_t[0]));
   }
   printf("\n");
   free(y);
   return 0;
 }
 
-
-float* euler_method(int t0, int y0, double delta_t){
+float* euler_method(int t0, int y0, float delta_t){
   int i, j;
-  int N = 10/delta_t;
-  float sum_exp = 1;
-  float* y = (float*) malloc(sizeof(float)*N); //Asignacion de memoria
-  for(i = 0 ; i < N ; i++){
-    for(j = 0 ; j <= i - 1 ; j++){
-      sum_exp += exp(-j*delta_t);
+  int N = 10 / delta_t;
+  float sum;
+  float* y = (float*) malloc(sizeof(float) * N - sizeof(float) * 9); //Asignacion de memoria
+  for(i = 10 ; i <= N ; i++){ //Desde 10 porque necesita empezar desde n=1, hasta n=10
+  	sum = 0;
+    for(j = 0 ; j <= i - 1 ; j++){//Sigue en j=0 porque necesita de esos valores para calcular los siguientes
+      sum += edo_original(j*delta_t);
     }
-    y[i] = -y0 + (delta_t*sum_exp);
+    y[i-10] = y0 + (delta_t * sum);
   }
   return y;
+}
+
+float edo_original(float t)
+{
+	return 9 * (powf(t, 2)) - 4 * t + 5;
+}
+float edo_resuelta(float t)
+{
+	return 3 * (powf(t, 3)) - 2 * (powf(t, 2)) + 5 * t + 4;
 }
