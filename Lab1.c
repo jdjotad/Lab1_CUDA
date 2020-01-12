f2d5cde26b28eb998c31acd3c0e6c608199855d9
Cristobal Abarca Olivares <cristoxea@localhost.localdomain>
2020-01-12 20:07:11 -0300

    Se adaptan a todos los deltas

--- Lab1.c
+++ Lab1.c
@@ -9,34 +9,42 @@ float edo_resuelta(float t);
 int main(){
   int t0;
   int y0;
-  int i;
+  int i, j;
   t0 = 0;
   y0 = 4;
-  float delta_t[6] = {pow(10,-1), pow(10,-2), pow(10,-3), pow(10,-4),
-                    pow(10,-5), pow(10,-6)};
+  float delta_t[6] = {pow(10, -1), pow(10, -2), pow(10, -3), pow(10, -4),
+                    pow(10, -5), pow(10, -6)};
   float* y;
-  y = euler_method(t0, y0, delta_t[0]);
-  for(i = 0 ; i <= 10/delta_t[0] - 10  ; i++)
+
+  for(j = 0 ; j < 6 ; j++)
   {
-  	printf("%f\n", (i + 10)*delta_t[0]);
-  	printf("y[%i]=%f   ,   %f\n", i, *(y+i), edo_resuelta((i + 10)*delta_t[0]));
+  	printf("*********************************\n");
+  	printf("Con delta = %f\n", delta_t[j]);
+  	printf("*********************************\n");
+  	y = euler_method(t0, y0, delta_t[j]);
+  	for(i = 0 ; i <= (10 / delta_t[j])  - (1 / delta_t[j]); i++)
+	{
+		printf("%f\n", (i + (1/delta_t[j])) * delta_t[j]);
+	    printf("y[%i]=%f   ,   %f\n", i, *(y + i), edo_resuelta((i + 1 / delta_t[j]) * delta_t[j]));
+	}
+	free(y);
   }
   printf("\n");
-  free(y);
   return 0;
 }
+
 
 float* euler_method(int t0, int y0, float delta_t){
   int i, j;
   int N = 10 / delta_t;
   float sum;
-  float* y = (float*) malloc(sizeof(float) * N - sizeof(float) * 9); //Asignacion de memoria
-  for(i = 10 ; i <= N ; i++){ //Desde 10 porque necesita empezar desde n=1, hasta n=10
+  float* y = (float*) malloc(sizeof(float) * N - (1 / delta_t - 1) * sizeof(float)); //Asignacion de memoria
+  for(i = 1 / delta_t ; i <= N ; i++){ //Desde 1 / delta_t porque necesita empezar desde n=1, hasta n=10
   	sum = 0;
     for(j = 0 ; j <= i - 1 ; j++){//Sigue en j=0 porque necesita de esos valores para calcular los siguientes
       sum += edo_original(j*delta_t);
     }
-    y[i-10] = y0 + (delta_t * sum);
+    y[i - (int) (1 / delta_t)] = y0 + (delta_t * sum);
   }
   return y;
 }
@@ -45,6 +53,7 @@ float edo_original(float t)
 {
 	return 9 * (powf(t, 2)) - 4 * t + 5;
 }
+
 float edo_resuelta(float t)
 {
 	return 3 * (powf(t, 3)) - 2 * (powf(t, 2)) + 5 * t + 4;
