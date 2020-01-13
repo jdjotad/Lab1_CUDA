39dda1f59f3b323e01de9f9ebeacb4fcb4a5e16a
jotad <juan.escarate@sansano.usm.cl>
2020-01-13 14:21:19 -0300

    mend

--- Lab1.c
+++ Lab1.c
@@ -33,7 +33,7 @@ int main(){
     	fprintf(fp, "%f\n", (i + (1/delta_t[j])) * delta_t[j]);
       fprintf(fp, "y[%i]=%f   ,   %f\n", i, *(y + i), edo_resuelta((i + 1 / delta_t[j]) * delta_t[j]));
     }
-    total_t = (double)(end_t - start_t)/ CLOCKS_PER_SEC;
+    total_t = (double)((end_t - start_t)/ CLOCKS_PER_SEC);
     fprintf(fp, "Tiempo que demora en CPU = %f\n", total_t);
 	free(y);
 
