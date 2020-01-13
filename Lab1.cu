c461fca48ccad84fcdca6441bbdd0aa226865b23
jotad <juan.escarate@sansano.usm.cl>
2020-01-13 14:19:14 -0300

    Push without 1_a

--- Lab1.cu
+++ Lab1.cu
@@ -4,30 +4,49 @@
 #include<math.h>
 #include<time.h>
 
-float* euler_method(int t0, int y0, float delta_t);
+__global__ void euler_method(	int t0, int y0, float delta_t)
+{
+	int i, j;
+	int N = int(10 / delta_t);
+	float sum;
+	float *y_dev;
+	// Guardar espacio en memoria GPU
+	cudaMalloc(&y_dev, sizeof(float * (N - (1 / delta_t - 1))));
+	for(i = 1 / delta_t ; i <= N ; i++){ //Desde 1 / delta_t porque necesita empezar desde n=1, hasta n=10
+		sum = 0;
+		for(j = 0 ; j <= i - 1 ; j++){//Sigue en j=0 porque necesita de esos valores para calcular los siguientes
+		  sum += edo_original(j*delta_t);
+		}
+		y[i - (int) (1 / delta_t)] = y0 + (delta_t * sum);
+		}
+	return y;
+}
+
 float edo_original(float t);
 float edo_resuelta(float t);
+
 int main(){
-  FILE *fp;
-  int t0;
-  int y0;
-  int i, j;
+	FILE *fp;
+	int t0;
+	int y0;
+  	int i, j;
   t0 = 0;
   y0 = 4;
   float delta_t[6] = {pow(10, -1), pow(10, -2), pow(10, -3), pow(10, -4),
                     pow(10, -5), pow(10, -6)};
-  float* y;
+  float *y;
   clock_t start_t, end_t, total_t;
-  fp = fopen("../1_a", "w");
+  fp = fopen("1_a_cuda", "w");
 
   for(j = 0 ; j < 6 ; j++)
   {
   	fprintf(fp, "*********************************\n");
   	fprintf(fp, "Con delta = %f\n", delta_t[j]);
   	fprintf(fp, "*********************************\n");
-    start_t = clock();
-  	y = euler_method(t0, y0, delta_t[j]);
-    end_t = clock();
+		// Guardar espacio en memoria CPU
+		y = (float*) malloc(sizeof(float * (N - (1 / delta_t - 1))));
+  	euler_method<<1,1>>(t0, y0, delta_t);
+		cudaMemcpy(y_dev, y, sizeof(sizeof(float * (N - (1 / delta_t - 1)))), cudaMemcpyDeviceToHost);
   	for(i = 0 ; i <= (10 / delta_t[j])  - (1 / delta_t[j]); i++)
     {
     	fprintf(fp, "%f\n", (i + (1/delta_t[j])) * delta_t[j]);
@@ -43,19 +62,6 @@ int main(){
 
 
 float* euler_method(int t0, int y0, float delta_t){
-  int i = 1 / delta_t;
-  int j = 0;
-  int N = 10 / delta_t;
-  float sum = 0;
-  float* y = (float*) malloc(sizeof(float) * N - (1 / delta_t - 1) * sizeof(float)); //Asignacion de memoria
-  for(j = 0 ; j <= i - 1 ; j++){
-    sum += edo_original(j*delta_t);
-  }
-  for(i = 1 / delta_t ; i <= N ; i++, j++){ //Desde 1 / delta_t porque necesita empezar desde n=1, hasta n=10
-      y[i - (int) (1 / delta_t)] = y0 + (delta_t * sum);
-      sum += edo_original(j*delta_t);
-  }
-  return y;
 }
 
 float edo_original(float t)
