4be8934b3bdd645c079b63e786018bebdee60026
jotad <juan.escarate@sansano.usm.cl>
2020-01-15 15:43:09 -0300

    Primera pregunta lista

--- Lab1_hybrid.cu
+++ Lab1_hybrid.cu
@@ -3,34 +3,28 @@
 #include<ctype.h>
 #include<math.h>
 #include<time.h>
-__device__ float edo_original(float t)
-{
-	return 9 * (powf(t, 2)) - 4 * t + 5;
-}
 
-__global__ void euler_method(float *y, float delta_t, int N)
+__global__ void euler_method(float *y, float *sum, float delta_t, int N)
 {
 	int y0 = 4;
 	int tId = threadIdx.x + blockIdx.x*blockDim.x;
-	float sum = 0;
 	if(tId <= N){
-		for(int i = 0, j = 0; i <= tId ; i++, j++){ //Desde 1 / delta_t porque necesita empezar desde n=1, hasta n=10
-				sum += edo_original(j*delta_t);
-				y[i] = y0 + (delta_t * sum);
-		}
+			y[tId] = y0 + delta_t * sum[tId];
 	}
 }
 
 float edo_resuelta(float t);
+float edo_original(float t);
+void sumatoria(float *sum, float delta_t, int N);
 
 int main(){
 	FILE *fp;
 	int i, j, N, counter = 0;
   float delta_t[6] = {powf(10, -1), powf(10, -2), powf(10, -3), powf(10, -4),
                     powf(10, -5), powf(10, -6)};
-  float *y, *y_dev;
+  float *y, *y_dev, *sum, *sum_dev;
 	int block_size, grid_size;
-  fp = fopen("../1_b", "w");
+  fp = fopen("../1_c", "w");
 
 	cudaEvent_t ct1, ct2;
 	float dt;
@@ -41,14 +35,21 @@ int main(){
   	fprintf(fp, "*********************************\n");
   	fprintf(fp, "Con delta = %f\n", delta_t[j]);
   	fprintf(fp, "*********************************\n");
-		block_size = 256; N = 10 / delta_t[j];
+
+		block_size = 256;
+		N = 10 / delta_t[j];
 		grid_size = (int)ceil((float)(N +1 )/ block_size);
 
 		cudaMalloc(&y_dev, sizeof(float) * (N + 1));
+		cudaMalloc(&sum_dev, sizeof(float) * (N + 1));
+		sum = (float*) malloc(sizeof(float) * (N + 1));
 		y = (float*) malloc(sizeof(float) * (N + 1));
 
+		sumatoria(sum, delta_t[j], N);
+
 		cudaEventRecord(ct1);
-  	euler_method<<<grid_size,block_size>>>(y_dev, delta_t[j], N);
+		cudaMemcpy(sum_dev, sum, (N + 1)*sizeof(float), cudaMemcpyHostToDevice);
+  	euler_method<<<grid_size,block_size>>>(y_dev, sum_dev, delta_t[j], N);
 		cudaEventRecord(ct2);
 		cudaMemcpy(y, y_dev, (N + 1)*sizeof(float), cudaMemcpyDeviceToHost);
 		cudaEventSynchronize(ct2);
@@ -61,9 +62,22 @@ int main(){
     }
 		counter++; printf("Tiempo que demora en GPU = %f [ms] para delta numero %d\n", dt, counter);
 		free(y);
+		free(sum);
 		cudaFree(y_dev);
   }
   return 0;
+}
+
+void sumatoria(float *sum, float delta_t, int N){
+	sum[0] = edo_original(0);
+	for(int i = 1; i <= N; i++){
+		sum[i] = sum[i-1] + edo_original(i*delta_t);
+	}
+}
+
+float edo_original(float t)
+{
+	return 9 * (powf(t, 2)) - 4 * t + 5;
 }
 
 float edo_resuelta(float t)
